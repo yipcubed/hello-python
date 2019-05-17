fdd4e8e50f4360fa3e914fa7489710ba4b5b798d
Yip Cubed <yipcubed@gmail.com>
2019-05-17 05:09:31 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# F1 to Open Shell
+#
+# git init
+# git add main.py
+# git config user.name "Yip Cubed"
+# git config user.email "yipcubed@gmail.com"
+# git commit -m "first commit"
+# git remote add origin git@github.com:yipcubed/hello-python.git
+# git push -u origin master
+
+print("Hello, World")
